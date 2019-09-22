b568abf0a61328118728a30e18f0cdb54aa0f118
Dustin Newman <dustinnewman98@gmail.com>
2019-09-22 15:49:52 -0700

    mongo no redis'

--- backend/sessions.js
+++ backend/sessions.js
@@ -1,11 +1,12 @@
-const redis = require("redis");
 const session = require("express-session");
-const RedisStore = require("connect-redis")(session)
-const client = redis.createClient(process.env.REDIS_URL || "redis://redis:6379")
+const MongoStore = require("connect-mongo")(session);
+const { mongoose } = require("./db");
 
 module.exports = session({
     secret: process.env.SESSION_SECRET,
-    // store: new RedisStore({ client }),
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    }),
     resave: false,
     saveUninitialized: true,
     cookie: {
